3947eede3ffaaa50f9d3c307984d07c4cd29b0f5
Gan Lab <gan-lab@mae-25207-l.sabu.mtu.edu>
2026-06-12 13:08:01 -0400

    second commit

--- offboard_control/offboard_node.py
+++ offboard_control/offboard_node.py
@@ -2,6 +2,7 @@
 import rclpy
 import threading
 from rclpy.node import Node
+import math
 
 from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleAttitudeSetpoint, VehicleLocalPosition
 from geometry_msgs.msg import PoseStamped
@@ -23,84 +24,126 @@ class OffboardControl(Node):
         self.attitude_setpoint_pub = self.create_publisher(VehicleAttitudeSetpoint,'/fmu/in/trajectory_setpoint',qos_profile)
 
         self.vehicle_command_pub = self.create_publisher(VehicleCommand,'/fmu/in/vehicle_command',qos_profile)
-        self.create_subscription(VehicleLocalPosition,'/fmu/out/vehicle_local_position', self.position_callback,qos_profile)
-        self.create_subscription(PoseStamped,'/vvhub_body_wrt_local/odom', self.position_vvcallback,qos_profile)
-
+        
         # Timer at 10Hz
         self.timer = self.create_timer(0.1, self.timer_callback)
-
-        self.offboard_setpoint_counter = 0
 
         self.get_logger().info("Offboard control node started")
         self.time = self.get_clock().now().nanoseconds // 1000
         self.time_at_arm= 0
+        
+        # counters
+        self.offboard_setpoint_counter = 0
+        self.path_counter_circle=0
+        self.path_counter_straight=0
 
-        self.pos_x=0
-        self.pos_y=0
-        self.pos_z=0
+        #constants
+        self.altitude=-0.5
+        self.radius=0.5
+        self.distance=0.5
 
-        self.vv_pose_x=0
-        self.vv_pose_y=0
-        self.vv_pose_z=0
-
+        # safty flags``
         self.land_request=False
+        self.ready2hover=True
+        self.disarm=False
+        self.path1=False
+        self.path2=False
         threading.Thread(target=self.keyboard_listener,daemon=True).start()
 
     def keyboard_listener(self):
+        print('keyboard listner started')
         while rclpy.ok():
             key=input()
-            if key.lower=='l':
-                 self.land_request=True
-                 #self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
-                 print('emergency landing')
-
-    def position_vvcallback(self, msg):
-
-        self.vv_pose_x=msg.pose.position.x
-        self.vv_pose_y=msg.pose.position.y
-        self.vv_pose_z=msg.pose.position.z
-
-    def position_callback(self,msg):
-        self.pos_x=msg.x
-        self.pos_y=msg.y
-        self.pos_z=msg.z
+            print(f'command recieved = {key}')
+            if key.lower()=='q':
+                self.land_request=True
+                self.ready2hover=False
+                self.disarm=False
+                self.path1=False
+                self.path2=False
+                print('emergency landing requested')
+            if key.lower()=='d':
+                self.land_request=False
+                self.ready2hover=False
+                self.disarm=True
+                self.path1=False
+                self.path2=False
+            if key.isdigit()==1:
+                self.land_request=False
+                self.ready2hover=False
+                self.disarm=False
+                self.path1=True
+                self.path2=False
+            if key.isdigit()==2:
+                self.land_request=False
+                self.ready2hover=False
+                self.disarm=False
+                self.path2=True
+                self.path1=False
 
     def timer_callback(self):
 
         # Publish offboard heartbeat
         self.publish_offboard_control_mode()
-
-        # Publish desired position
-        self.publish_trajectory_setpoint()
-        #self.publish_attitude_setpoint()
         
         self.offboard_setpoint_counter += 1
-        #print(f'at  counter {self.offboard_setpoint_counter} position of drone is from px4[{self.pos_x:.3f},{self.pos_y:.3f},{self.pos_z:.3f}] and vision hub is [{self.vv_pose_x:.3f},{self.vv_pose_y:.3f},{self.vv_pose_z:.3f}]')
 
         # Wait a bit before arming and switching mode
-        if self.offboard_setpoint_counter == 20:# and self.time_passed>2:
-            self.time_at_arm=self.get_clock().now().nanoseconds // 1000
+        if self.ready2hover:
+            if self.offboard_setpoint_counter==20:
+                self.get_logger().info("Switching to OFFBOARD mode")
+                print(f'counter at {self.offboard_setpoint_counter}')
 
-            self.get_logger().info("Switching to OFFBOARD mode")
+                # Set OFFBOARD mode
+                self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE,param1=1.0,param2=6.0)
 
-            # Set OFFBOARD mode
-            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE,param1=1.0,param2=6.0)
+                print(f'command send at  counter {self.offboard_setpoint_counter}')
+                self.get_logger().info("Arming")
 
-            print(self.offboard_setpoint_counter)
-            self.get_logger().info("Arming")
-
-            # Arm
-            self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM,param1=1.0)
+                # Arm
+                self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM,param1=1.0)
             
+            elif self.offboard_setpoint_counter>30:
+                # Publish desired position
+                self.publish_trajectory_setpoint(self.altitude)
 
-        if self.land_request:
+        elif self.path1:
+            self.path_counter_straight+=1
+
+        
+        elif self.path2:
+            self.circular_path_setpoint(self.altitude)
+            self.path_counter_circle+=1
+
+        elif self.land_request:
             self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
             print(f"now landing {self.offboard_setpoint_counter}")
-
-        if self.offboard_setpoint_counter >= 130:
+        
+        elif self.disarm:
             self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM,param1=0.0)
-            print("disarming")
-            print(self.offboard_setpoint_counter)
+    
+    def straight_path_setpoint(self,z):
+        # angle increment by 0.02 rad
+        angle = self.path_counter_straight*0.02
+        x = self.distance*math.sin(angle)
+        msg = TrajectorySetpoint()
+        msg.position = [x,0,z]
+        msg.yaw=0.0
+        self.trajectory_setpoint_pub(msg)
+
+    def circular_path_setpoint(self, z):
+        # angle increment by 0.02 rad
+        angle= self.path_counter_circle*0.02
+        x=self.radius*math.cos(angle)
+        y=self.radius*math.sin(angle)
+
+        msg = TrajectorySetpoint()
+        msg.position=[x,y,z]
+        #vx = -R*math.sin(angle)
+        #vy = R*math.cos(angle)
+        #yaw = math.atan2(vy,vx)
+        msg.yaw=0.0
+        self.trajectory_setpoint_pub(msg)
 
     def publish_offboard_control_mode(self):
 
@@ -116,14 +159,14 @@ class OffboardControl(Node):
 
         self.offboard_control_mode_pub.publish(msg)
 
-    def publish_trajectory_setpoint(self):
+    def publish_trajectory_setpoint(self,z):
 
         msg = TrajectorySetpoint()
 
         msg.timestamp = self.get_clock().now().nanoseconds // 1000
 
         # Position setpoint
-        msg.position = [0.0, 0.0, -0.5]
+        msg.position = [0.0, 0.0, z]
 
         # Yaw angle
         msg.yaw = 0.0
