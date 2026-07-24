d003ab83964ee745c6bb91f3b19bf82699d1b93e
Gan Lab <gan-lab@mae-25207-l.sabu.mtu.edu>
2026-07-24 13:01:40 -0400

    five commit

--- offboard_control/offboard_node.py
+++ offboard_control/offboard_node.py
@@ -4,7 +4,7 @@ import threading
 from rclpy.node import Node
 import math
 
-from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleAttitudeSetpoint, VehicleLocalPosition
+from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleLocalPosition
 from geometry_msgs.msg import PoseStamped
 
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
@@ -21,12 +21,14 @@ class OffboardControl(Node):
         self.offboard_control_mode_pub = self.create_publisher(OffboardControlMode,'/fmu/in/offboard_control_mode',qos_profile)
 
         self.trajectory_setpoint_pub = self.create_publisher(TrajectorySetpoint,'/fmu/in/trajectory_setpoint',qos_profile)
-        self.attitude_setpoint_pub = self.create_publisher(VehicleAttitudeSetpoint,'/fmu/in/trajectory_setpoint',qos_profile)
+
+        self.create_subscription(VehicleLocalPosition,"/fmu/out/vehicle_local_position",self.curr_pose,qos_profile)
+        #self.attitude_setpoint_pub = self.create_publisher(VehicleAttitudeSetpoint,'/fmu/in/trajectory_setpoint',qos_profile)
 
         self.vehicle_command_pub = self.create_publisher(VehicleCommand,'/fmu/in/vehicle_command',qos_profile)
         
         # Timer at 10Hz
-        self.timer = self.create_timer(0.1, self.timer_callback)
+        self.timer = self.create_timer(0.02, self.timer_callback)
 
         self.get_logger().info("Offboard control node started")
         self.time = self.get_clock().now().nanoseconds // 1000
@@ -36,6 +38,10 @@ class OffboardControl(Node):
         self.offboard_setpoint_counter = 0
         self.path_counter_circle=0
         self.path_counter_straight=0
+
+        self.x =0.0
+        self.y =0.0
+        self.z =0.0
 
         #constants
         self.altitude=-0.5
@@ -48,6 +54,8 @@ class OffboardControl(Node):
         self.disarm=False
         self.path1=False
         self.path2=False
+        self.target_point_follower=False
+        self.hold=False
         threading.Thread(target=self.keyboard_listener,daemon=True).start()
 
     def keyboard_listener(self):
@@ -61,6 +69,8 @@ class OffboardControl(Node):
                 self.disarm=False
                 self.path1=False
                 self.path2=False
+                self.target_point_follower=False
+                self.hold=False
                 print('emergency landing requested')
             if key.lower()=='d':
                 self.land_request=False
@@ -68,18 +78,54 @@ class OffboardControl(Node):
                 self.disarm=True
                 self.path1=False
                 self.path2=False
+                self.hold=False
+                self.target_point_follower=False
+                            
+            if key.lower()=='h':
+                self.land_request=False
+                self.ready2hover=False
+                self.disarm=False
+                self.path1=False
+                self.path2=False
+                self.hold=True
+                self.target_point_follower=False
+                self.x =self.curr_x
+                self.y =self.curr_y
+                self.z =self.curr_z
             if key.isdigit()==1:
                 self.land_request=False
                 self.ready2hover=False
                 self.disarm=False
                 self.path1=True
                 self.path2=False
+                self.hold=False
+                self.target_point_follower=False
             if key.isdigit()==2:
                 self.land_request=False
                 self.ready2hover=False
                 self.disarm=False
                 self.path2=True
                 self.path1=False
+                self.hold=False
+                self.target_point_follower=False
+            if key.isdigit()==3:
+                self.land_request=False
+                self.ready2hover=False
+                self.disarm=False
+                self.path2=False
+                self.path1=False
+                self.hold=False
+                self.target_point_follower=True
+
+    def target_point_listener(self,msg):
+        self.target_set_point_x=msg[0]
+        self.target_set_point_y=msg[1]
+
+    def curr_pose(self, msg):
+        self.curr_x = msg.x
+        self.curr_y = msg.y
+        self.curr_z = msg.z
+
 
     def timer_callback(self):
 
@@ -90,7 +136,7 @@ class OffboardControl(Node):
 
         # Wait a bit before arming and switching mode
         if self.ready2hover:
-            if self.offboard_setpoint_counter==20:
+            if self.offboard_setpoint_counter==150:
                 self.get_logger().info("Switching to OFFBOARD mode")
                 print(f'counter at {self.offboard_setpoint_counter}')
 
@@ -103,7 +149,7 @@ class OffboardControl(Node):
                 # Arm
                 self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM,param1=1.0)
             
-            elif self.offboard_setpoint_counter>50:
+            elif self.offboard_setpoint_counter>300:
                 # Publish desired position
                 self.publish_trajectory_setpoint()
 
@@ -122,7 +168,16 @@ class OffboardControl(Node):
         
         elif self.disarm:
             self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM,param1=0.0)
-    
+        elif self.target_point_follower:
+            self.target_point(self.altitude)
+        elif self.hold:
+            self.hover(self.x,self.y,self.z)
+    def hover(self,x,y,z):
+        msg = TrajectorySetpoint()
+        msg.position = [x,y,z]
+        msg.yaw=0.0
+        self.trajectory_setpoint_pub.publish(msg)
+
     def straight_path_setpoint(self,z):
         # angle increment by 0.02 rad
         angle = self.path_counter_straight*0.02
@@ -130,6 +185,16 @@ class OffboardControl(Node):
         msg = TrajectorySetpoint()
         msg.position = [x,0.0,z]
         msg.yaw=0.0
+        self.trajectory_setpoint_pub.publish(msg)
+
+    def target_point(self, z):
+        
+        x = self.target_set_point_x
+        y = self.target_set_point_y
+
+        msg = TrajectorySetpoint()
+        msg.position = [x,y,z]
+        
         self.trajectory_setpoint_pub.publish(msg)
 
     def circular_path_setpoint(self, z):
